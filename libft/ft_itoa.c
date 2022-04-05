a8c37b432f2641a860756ec6dfea43139d2144a4
tlahin <teemu.lahin@gmail.com>
2022-04-06 02:08:13 +0300

    Gnl

---
+++ libft/ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlahin <tlahin@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 12:14:53 by tlahin            #+#    #+#             */
+/*   Updated: 2021/12/13 12:52:08 by tlahin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_len(int n)
+{
+	int	len;
+
+	if (n == 0)
+		return (1);
+	len = 0;
+	if (n < 0)
+		len++;
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		i;
+
+	i = ft_len(n);
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == -2147483648)
+		return (ft_strcpy(str, "-2147483648"));
+	if (n == 0)
+		str[0] = 48;
+	if (n < 0 && n > -2147483648)
+	{
+		str[0] = '-';
+		n = n * -1;
+	}
+	while (n > 0)
+	{
+		str[i] = 48 + (n % 10);
+		n = n / 10;
+		i--;
+	}
+	str[ft_strlen(str) + 1] = '\0';
+	return (str);
+}
